2c5d8cf4c2179655378098ed83ac9361f04bfe2a
Евгений Наволоков <goustg62@gmail.com>
2026-07-01 15:54:20 +0300

    Add import for css and router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { router } from './router/index.js'
 import App from './App.vue'
 
-// Initialize Telegram WebApp
-if (window.Telegram?.WebApp) {
-  window.Telegram.WebApp.ready()
-  window.Telegram.WebApp.expand()
-}
+import './assets/style.css'
 
 const app = createApp(App)
-const pinia = createPinia()
 
-app.use(pinia)
+app.use(createPinia())
+app.use(router)
 app.mount('#app')
