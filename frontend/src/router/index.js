5eac77d620c32dafab7a9ec7e766ad6af3df3995
Евгений Наволоков <goustg62@gmail.com>
2026-07-11 17:11:43 +0300

    Add routes for strategies overview and single strategy

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
+import Strategy from '../pages/Strategy.vue'
+import Strategies from '../pages/Strategies.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/map/:id',
+    name: 'Strategies',
+    component: Strategies
+  },
+  {
+    path: '/strategy/:id',
+    name: 'Strategy',
+    component: Strategy
   }
 ]
 
